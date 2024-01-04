903979556b41d57ae5906dfa59d9f143a224ca7b
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-04 18:51:22 -0300

    inicializing project

---
+++ src/test/java/br/com/AppHR/AppHR/AppHrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AppHR.AppHR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
