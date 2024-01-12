14427512b28c66e50552bff63b8e8ddf6c0b130b
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-12 19:18:50 -0300

    hibernate jpa config

--- src/main/java/br/com/AppHR/AppHR/DataConfiguration.java
+++ src/main/java/br/com/AppHR/AppHR/DataConfiguration.java
@@ -5,6 +5,9 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
 public class DataConfiguration {
@@ -12,10 +15,19 @@ public class DataConfiguration {
 	DataSource datasource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/AppHR"); //AppHR?serverTimezone=true&serverTimezone=UTC
+		dataSource.setUrl("jdbc:mysql://localhost:3306/AppHR"); // AppHR?serverTimezone=true&serverTimezone=UTC
 		dataSource.setUsername("root");
 		dataSource.setPassword("162607");
 		return dataSource;
-		              
+	}
+
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+		adapter.setGenerateDdl(true);
+		adapter.setShowSql(true);
+		adapter.setPrepareConnection(true);
+		return null;
 	}
 }
